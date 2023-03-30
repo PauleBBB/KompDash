5e30ed401e8ea0c3a6d317dcd4ef73348d711ed3
PauleBBB <33276061+PauleBBB@users.noreply.github.com>
2023-03-30 10:51:33 +0200

    Update StreamLit.py

--- StreamLit.py
+++ StreamLit.py
@@ -6,8 +6,8 @@ st.set_page_config(layout="wide")
 import requests
 import pandas as pd
 import numpy as np
-import plotly.express as px
-#import plotly as px
+#import plotly.express as px
+import plotly as px
 import streamlit as st
 
 from vega_datasets import data
@@ -16,20 +16,20 @@ from vega_datasets import data
 #
 # Code to import  the data goes here
 # Cache the dataframe so it's only loaded once
-@st.experimental_memo
+@st.cache_data
 def load_data():
     job_data = pd.read_csv("job_data.csv", sep=";")
 
     return job_data
 
-@st.experimental_memo
+@st.cache_data
 def load_geo():
     polygons = requests.get(
     "https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/4_kreise/2_hoch.geo.json"
     ).json()
     return polygons
 
-@st.experimental_memo
+@st.cache_data
 def load_jobs():
     Jobs = data.iowa_electricity()
     Jobs = Jobs.replace("Fossil Fuels", "Altenpfleger")
@@ -96,7 +96,7 @@ with st.container():
     source = source[["Kompetenz_Begriff", "Relevanz"]]
 
     if datenbasis:
-        st.dataframe(source)
+        st.dataframe(source, use_container_width=True)
     komp_chart = alt.Chart(source).mark_bar().encode(
            # x='sum(yield):Q',
             x='Relevanz:Q',
@@ -123,7 +123,7 @@ with st.container():
         )
 
 
-    fig = px.choropleth(
+    fig = px.express.choropleth(
         df,
         geojson=polygons,
         locations="Relevanz",
@@ -145,14 +145,161 @@ with st.container():
         x="Jahr:T",
         y=alt.Y("Stellenanzeigen:Q", stack=None),
         color="job:N")
-    st.altair_chart(alt_chart, use_container_width=True)
+    st.altair_chart(alt_chart, theme=None, use_container_width=True)
 
 
+############test
+@st.cache_data
+def load_data_isco_komp_data():
+    isco_komp_data = pd.read_csv("isco_komp_data.csv", sep=";")
+
+    return isco_komp_data
 
 
+def create_chart(df, beruf):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    # Ensures reproducibility of random numbers
+    #rng = np.random.default_rng(123)
+
+    def get_label_rotation(angle, offset):
+        # Rotation must be specified in degrees :(
+        rotation = np.rad2deg(angle + offset)
+        if angle <= np.pi:
+            alignment = "right"
+            rotation = rotation + 180
+        else:
+            alignment = "left"
+        return rotation, alignment
+
+    def add_labels(angles, values, labels, offset, ax):
+        # This is the space between the end of the bar and the label
+        padding = 4
+
+        # Iterate over angles, values, and labels, to add all of them.
+        for angle, value, label, in zip(angles, values, labels):
+            angle = angle
+
+            # Obtain text rotation and alignment
+            rotation, alignment = get_label_rotation(angle, offset)
+
+            # And finally add the text
+            ax.text(
+                x=angle,
+                y=value + padding,
+                s=label,
+                ha=alignment,
+                va="center",
+                rotation=rotation,
+                rotation_mode="anchor",
+                fontsize = 18
+            )
+
+    ###check
+    OFFSET = np.pi / 2
+
+    # All this part is like the code above
+    GROUPS_SIZE = df['Kompetenz'].value_counts().tolist()
+    df = df.groupby('Kompetenz').apply(lambda x: x.sort_values(by='Kompetenz_Begriff')).reset_index(drop=True)
+    df = df.assign(freq=df.groupby('Kompetenz')['Kompetenz'].transform('count')) \
+        .sort_values(by=['freq', 'Kompetenz'], ascending=[False, True])
 
 
+    VALUES = df["Relevanz"].values
+    LABELS = df["Kompetenz_Begriff"].values
+    GROUP = df["Kompetenz"].values
+    KOMPETENZ = df["Kompetenz"].unique().tolist()
+    if beruf == "Rank_Total 825_Medizin-_Orthopädie- und Rehatechnik":
+        beruf = "Rank_Total 825_Medizin- Orthopädie- und Rehatechnik"
+    if beruf == "Rank_Total 816_Psychologie_nichtärztl. Psychotherapie":
+        beruf = "_816_Psychologie, nichtärztl. Psychotherapie"
+    if beruf == "Rank_Total 813_Gesundh._Krankenpfl._Rettungsd.Geburtsh.":
+        beruf = "_813_Gesundh., Krankenpfl., Rettungsd., Geburtsh."
+    beruf = beruf.split("_")[2]
+    beruf = beruf.replace(" ", "\n")
+
+    PAD = 3
+    ANGLES_N = len(VALUES) + PAD * len(np.unique(GROUP))
+    ANGLES = np.linspace(0, 2 * np.pi, num=ANGLES_N, endpoint=False)
+    WIDTH = (2 * np.pi) / len(ANGLES)
+
+    offset = 0
+    IDXS = []
+
+    for size in GROUPS_SIZE:
+        IDXS += list(range(offset + PAD, offset + size + PAD))
+        offset += size + PAD
+
+    fig, ax = plt.subplots(figsize=(18, 25), subplot_kw={"projection": "polar"})
+    ax.set_theta_offset(OFFSET)
+    ax.set_ylim(-30, 100)
+    ax.set_frame_on(False)
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    #COLORS = [f"C{i}" for i, size in enumerate(GROUPS_SIZE) for _ in range(size)]
+    index_ueberfachlich = KOMPETENZ.index("überfachlich")
+    COLORS = []
+    color_ueberfachlich = '#CC3A45'
+    colors_fachlich = ["#003663", "#3A8FEC", "#CDDEF2"]
+    colors_fachlich.insert(index_ueberfachlich, color_ueberfachlich)
+    for i, size in enumerate(GROUPS_SIZE):
+        if i == index_ueberfachlich:
+            COLORS.extend([color_ueberfachlich for _ in range(size)])
+        else:
+            COLORS.extend([colors_fachlich[i] for _ in range(size)])
 
 
+    ax.bar(
+        ANGLES[IDXS], VALUES, width=WIDTH, color=COLORS,
+        edgecolor="white", linewidth=2, label=GROUP
+    )
 
+    add_labels(ANGLES[IDXS], VALUES, LABELS, OFFSET, ax)
+
+    import matplotlib.patches as mpatches
+    handle_list = []
+    for i in range(len(GROUPS_SIZE)):
+        handle_list.append(mpatches.Patch(color=colors_fachlich[i], label="fachlich: "+KOMPETENZ[i] if i != index_ueberfachlich else KOMPETENZ[i]))
+
+    fig.legend(handles=handle_list, loc='lower left', bbox_to_anchor=(1, 0.2), title='Kompetenz', fontsize=18, title_fontsize='20', fancybox=True, edgecolor="#333333")
+    # fig.legend(handles=[komp1, komp2, komp3, komp4], loc='lower right', title='Kompetenz')
+
+    # Extra customization below here --------------------
+
+    # This iterates over the sizes of the groups adding reference
+    # lines and annotations.
+
+    offset = 0
+    for group, size in zip(["A", "B", "C", "D"], GROUPS_SIZE):
+        # Add line below bars
+        x1 = np.linspace(ANGLES[offset + PAD], ANGLES[offset + size + PAD - 1], num=25)
+        ax.plot(x1, [-5] * 25, color="#333333")
+
+        # Add text to indicate group
+        # ax.text(
+        #     np.mean(x1), -20, group, color="#333333", fontsize=14,
+        #     fontweight="bold", ha="center", va="center"
+        # )
+
+        # Add reference lines at 20, 40, 60, and 80
+        x2 = np.linspace(ANGLES[offset], ANGLES[offset + PAD - 1], num=25)
+        ax.plot(x2, [20] * 25, color="#bebebe", lw=0.8)
+        ax.plot(x2, [40] * 25, color="#bebebe", lw=0.8)
+        ax.plot(x2, [60] * 25, color="#bebebe", lw=0.8)
+        ax.plot(x2, [80] * 25, color="#bebebe", lw=0.8)
+
+        offset += size + PAD
+
+    plt.text(0.5, 0.5, beruf, horizontalalignment='center', color="#333333", fontsize=20, fontweight="bold",
+             verticalalignment='center', transform=ax.transAxes)
+
+    plt.tight_layout()
+
+
+with st.container():
+    create_chart(load_data_isco_komp_data(), "Rank_Total 825_Medizin-_Orthopädie- und Rehatechnik")
 
